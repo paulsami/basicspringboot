4095a9c358afb664d0536e1b50c146e002264681
paulsami <paulsami@gmail.com>
2022-03-23 22:34:39 +0530

    added

--- eureka-server/src/main/java/com/sami/boot/server/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/sami/boot/server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.bootcamp;
+package com.sami.boot.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/sami/boot/server/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/sami/boot/server/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bootcamp;
+package com.sami.boot.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
