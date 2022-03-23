bcd23eb49672072ceeca23eb966e75fc26fec39b
paulsami <paulsami@gmail.com>
2022-03-23 22:38:17 +0530

    updated

--- config-server/src/main/java/com/sami/boot/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/sami/boot/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.bootcamp.configserver;
+package com.sami.boot.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- config-server/src/test/java/com/sami/boot/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/sami/boot/configserver/ConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bootcamp.configserver;
+package com.sami.boot.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
